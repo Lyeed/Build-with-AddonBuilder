f1fac9c13b922484281739d1bf91cd54132acced
LyeedoO <gautier.jousset@epitech.eu>
2018-04-08 12:22:32 +0300

    fix

--- Install.bat
+++ Install.bat
@@ -9,9 +9,9 @@ xcopy "%~dp0data\*.bat" "%appdata%\AddonBuilderContextualMenu" /Y /S /I /Q
 
 reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual" /VE /D "Build with AddonBuilder" /F
 reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual" /v "Icon" /D "%AddonBuilder%,0" /F
-reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual\command" /VE /D "\"%appdata%\Build-with-AddonBuilder\Build.bat\" \"%AddonBuilder%\" \"%%1\"" /F
+reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual\command" /VE /D "\"%appdata%\AddonBuilderContextualMenu\Build.bat\" \"%AddonBuilder%\" \"%%1\"" /F
 
 reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual_packOnly" /VE /D "Build with AddonBuilder (pack only)" /F
 reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual_packOnly" /v "Icon" /D "%AddonBuilder%,0" /F
-reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual_packOnly\command" /VE /D "\"%appdata%\Build-with-AddonBuilder\BuildWithArgs.bat\" \"%AddonBuilder%\" \"%%1\" -packonly" /F
+reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual_packOnly\command" /VE /D "\"%appdata%\AddonBuilderContextualMenu\BuildWithArgs.bat\" \"%AddonBuilder%\" \"%%1\" -packonly" /F
 reg add "HKCU\Software\Classes\Directory\shell\AddonBuilderContextual_packOnly" /v "Extended" /F
